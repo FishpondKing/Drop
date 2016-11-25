78ba776ff3755edc03ef8a172a0b3542e0a55e43
FishpondKing <song511653502@gmail.com>
2016-11-25 10:42:28 +0800

    Add Dormitory Select View

---
+++ app/src/main/java/com/fishpondking/android/drop/activity/DormitorySelectActivity.java
@@ -0,0 +1,32 @@
+package com.fishpondking.android.drop.activity;
+
+import android.os.Bundle;
+
+import com.avos.avoscloud.AVException;
+import com.fishpondking.android.drop.R;
+import com.fishpondking.android.drop.utils.BaseActivity;
+
+/**
+ * Author: FishpondKing
+ * Date: 2016/11/25:9:29
+ * Email: song511653502@gmail.com
+ * Description:
+ */
+
+public class DormitorySelectActivity extends BaseActivity {
+
+    @Override
+    protected void initVariables() {
+
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) throws AVException {
+        setContentView(R.layout.activity_dormitory_select);
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
